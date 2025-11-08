09b48214771747086df29b24b8120ab89024c15a
ahmedsawy1 <sawy22417@gmail.com>
2025-11-08 10:37:40 +0200

    fix wrong path bug

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -5,10 +5,16 @@ class BaseController:
     
     def __init__(self):
         self.app_settings = get_settings()
-        # To get the current file directory => src/
+        # To get the current file directory => src/controllers
         self.build_dir = os.path.dirname(os.path.abspath(__file__))
 
+        # To get the project root directory => src/
+        self.project_root_dir = os.path.dirname(self.build_dir)
+
         # To get the files directory => src/assets/files
-        self.files_dir = os.path.join(self.build_dir, "assets/files")
+        self.files_dir = os.path.join(self.project_root_dir, "assets", "files")
         # Make the same thing but top is better because it will be good for all operating systems
         # self.files_dir = self.build_dir + "/assets/files"
+
+        if not os.path.exists(self.files_dir):
+            os.makedirs(self.files_dir, exist_ok=True)
