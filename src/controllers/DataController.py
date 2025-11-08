87e9b3a56eefd051a134f91e18511ad72ae19034
ahmedsawy1 <sawy22417@gmail.com>
2025-11-08 08:28:36 +0200

    adding response enums

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,4 +1,5 @@
 from .BaseController import BaseController
+from enums import ResponseMessageEnums
 from fastapi import UploadFile
 
 # DataController extends BaseController to provide data-specific operations.
@@ -8,9 +9,18 @@ class DataController(BaseController):
 
     def validate_uploaded_file(self, file: UploadFile):
         if file.content_type not in self.app_settings.FILE_ALLOWED_EXTENSIONS:
-            return False
+            return {
+                "success": False,
+                "error": ResponseMessageEnums.FILE_TYPE_NOT_ALLOWED.value
+            }
 
         if file.size > self.app_settings.FILE_MAX_SIZE:
-            return False
+            return {
+                "success": False,
+                "error": ResponseMessageEnums.FILE_SIZE_TOO_LARGE.value
+            }
 
-        return True
+        return {
+            "success": True,
+            "message": ResponseMessageEnums.FILE_UPLOADED_SUCCESSFULLY.value
+        }
---
+++ src/enums/ResponseEnums.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class ResponseMessageEnums(Enum):
+    FILE_UPLOADED_SUCCESSFULLY = "File uploaded successfully"
+    FILE_UPLOAD_FAILED = "File uploaded failed"
+    FILE_UPLOAD_IN_PROGRESS = "File uploaded in progress"
+    FILE_UPLOAD_COMPLETED = "File uploaded completed"
+    FILE_TYPE_NOT_ALLOWED = "File type not allowed"
+    FILE_SIZE_TOO_LARGE = "File size too large"
